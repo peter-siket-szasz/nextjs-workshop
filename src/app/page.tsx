268eef43b5c8608f898f7dc5e7b9a3f36cd1ffa6
Demi Rachel Brabender <rachel.brabender@netlight.com>
2023-11-07 10:03:31 +0100

    chakra and some components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import Header from "./components/Header";
-import ButtonLink from "./components/ButtonLink";
-import GamePageHeader from "./components/GamePageHeader";
+import { Box, Button } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <>
-      <GamePageHeader isRoot={true} />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+      <Navbar />
+      <Box width="100vw" height="90vh" bgColor="white" textAlign='center'>
+        <Box margin='center' display='inline-grid'>
+          <Button>New Quiz</Button>
+          <Button>Join Quiz</Button>
+        </Box>
+      </Box>
     </>
   );
 }
