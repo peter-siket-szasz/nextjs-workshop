ed46b1548eef7bc51a8abe9e64d7685557b497ad
Demi Rachel Brabender <rachel.brabender@netlight.com>
2023-11-07 18:00:42 +0100

    Navbar and Background Image

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+import NextImage from 'next/image';
+import netlightLogo from '../../../public/images/boid.png';
+
+export default function Navbar() {
+  return (
+    <>
+      <Box bg="gray.100" px={4}>
+        <Flex h={16} alignItems="center" justifyContent="space-between">
+          <Box width="50px" alignItems="center">
+            <NextImage src={netlightLogo} alt="logo" />
+          </Box>
+          <Flex alignItems="center">
+            <Box>
+              <Text as='i' fontSize="md">Amazing game incomming ...</Text>
+            </Box>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
-import { Box, Button } from '@chakra-ui/react';
-import Navbar from '../components/Navbar';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import Navbar from './components/Navbar';
+import flock from '../../public/images/flock.png';
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Box width="100vw" height="90vh" bgColor="white" textAlign='center'>
-        <Box margin='center' display='inline-grid'>
-          <Button>New Quiz</Button>
-          <Button>Join Quiz</Button>
-        </Box>
+      <Box width="100vw" height="90vh">
+        <div
+          style={{
+            backgroundImage: `url('${flock.src}')`,
+            width: '90%',
+            height: '90%',
+            position: 'absolute',
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+          }}
+        ></div>
+        <Flex alignItems="center">
+          <Box>
+            <Text fontSize="xl">You have the choice</Text>
+          </Box>
+          <Box margin="center" display="inline-grid">
+            <Button>New Quiz</Button>
+            <Button>Join Quiz</Button>
+          </Box>
+        </Flex>
       </Box>
     </>
   );
